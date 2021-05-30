1670d9ea8623eef4aca3bafa53e8a677936f3db2
Jeroen Hoekx <jeroen@hoekx.be>
2021-05-30 13:53:00 +0200

    Add UI (WIP)

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    entry: {
+        lib: './js/lib.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+        ]
+    }
+}
