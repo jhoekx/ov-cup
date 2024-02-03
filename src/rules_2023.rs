4ca04521ecf2d691ed536c6b0242cf4cf765d25a
Jeroen Hoekx <jeroen@hoekx.be>
2024-02-03 16:48:11 +0100

    Handle people changing age class mid-season

--- src/rules_2023.rs
+++ src/rules_2023.rs
@@ -1,14 +1,16 @@
 // SPDX-FileCopyrightText: 2023 Jeroen Hoekx
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use anyhow::bail;
 use itertools::Itertools;
 use regex::Regex;
-use rusqlite::params;
+use rusqlite::{params, Connection};
 
-use crate::{db::Database, total_seconds, Performance, RankingEntry, RankingScore, COURSES};
+use crate::{
+    db::Database, total_seconds, Performance, RankingEntry, RankingScore, CLASS_CHANGES, COURSES,
+};
 
 pub(crate) fn calculate_ranking(
     db: &dyn Database,
@@ -30,7 +32,77 @@ pub(crate) fn calculate_ranking(
         .filter_map(|event_id| event_id.ok())
         .collect();
 
-    let (age_class, course) = get_course(&age_class)?;
+    let mut results = calculate_performances(&conn, &cup, season, &age_class)?;
+
+    // Handle people changing class mid-season (only for forest cup)
+    if cup == "forest-cup" {
+        if let Some(other_class) = CLASS_CHANGES.get(&age_class as &str) {
+            let older_performances = calculate_performances(&conn, &cup, season, other_class)?;
+            // Add all older performances of runners in the real results
+            let all_runners: HashSet<String> = results.iter().map(|p| p.name.clone()).collect();
+            for performance in older_performances {
+                if all_runners.contains(&performance.name) {
+                    results.push(performance);
+                }
+            }
+        }
+    }
+
+    // Calculate the total scores per runner
+    let mut ranking: Vec<RankingEntry> = Vec::new();
+    for (name, runner_results) in &results
+        .into_iter()
+        .group_by(|result| result.name.to_owned())
+    {
+        let runner_results: Vec<Performance> = runner_results.collect();
+        let mut scores: Vec<u32> = runner_results.iter().map(|result| result.score).collect();
+        scores.sort_unstable();
+        scores.reverse();
+        let total_score: u32 = scores.iter().take(events_count).sum();
+
+        let ranking_scores: Vec<RankingScore> = runner_results
+            .iter()
+            .map(|performance| RankingScore {
+                event_id: performance.event_id,
+                score: Some(performance.score),
+                place: Some(performance.position),
+            })
+            .collect();
+
+        ranking.push(RankingEntry {
+            name,
+            club: runner_results
+                .last()
+                .map_or("".to_owned(), |performance| performance.club.to_string()),
+            total_score,
+            scores: events
+                .iter()
+                .map(|&event_id| {
+                    ranking_scores
+                        .iter()
+                        .find(|&score| score.event_id == event_id)
+                        .copied()
+                        .unwrap_or(RankingScore {
+                            event_id,
+                            score: None,
+                            place: None,
+                        })
+                })
+                .collect(),
+        })
+    }
+    ranking.sort_by_key(|entry| entry.total_score);
+    ranking.reverse();
+    Ok(ranking)
+}
+
+fn calculate_performances(
+    conn: &Connection,
+    cup: &str,
+    season: i16,
+    age_class: &str,
+) -> anyhow::Result<Vec<Performance>> {
+    let (age_class, course) = get_course(age_class)?;
     let performance_filter = PerformanceFilter::new(age_class);
 
     // Find all results in the courses of the requested category
@@ -96,53 +168,7 @@ pub(crate) fn calculate_ranking(
             / total_seconds(result.time);
         Performance { score, ..result }
     });
-
-    // Calculate the total scores per runner
-    let mut ranking: Vec<RankingEntry> = Vec::new();
-    for (name, runner_results) in &results
-        .into_iter()
-        .group_by(|result| result.name.to_owned())
-    {
-        let runner_results: Vec<Performance> = runner_results.collect();
-        let mut scores: Vec<u32> = runner_results.iter().map(|result| result.score).collect();
-        scores.sort_unstable();
-        scores.reverse();
-        let total_score: u32 = scores.iter().take(events_count).sum();
-
-        let ranking_scores: Vec<RankingScore> = runner_results
-            .iter()
-            .map(|performance| RankingScore {
-                event_id: performance.event_id,
-                score: Some(performance.score),
-                place: Some(performance.position),
-            })
-            .collect();
-
-        ranking.push(RankingEntry {
-            name,
-            club: runner_results
-                .last()
-                .map_or("".to_owned(), |performance| performance.club.to_string()),
-            total_score,
-            scores: events
-                .iter()
-                .map(|&event_id| {
-                    ranking_scores
-                        .iter()
-                        .find(|&score| score.event_id == event_id)
-                        .copied()
-                        .unwrap_or(RankingScore {
-                            event_id,
-                            score: None,
-                            place: None,
-                        })
-                })
-                .collect(),
-        })
-    }
-    ranking.sort_by_key(|entry| entry.total_score);
-    ranking.reverse();
-    Ok(ranking)
+    Ok(results.collect())
 }
 
 fn get_course(age_class: &str) -> anyhow::Result<(String, String)> {
